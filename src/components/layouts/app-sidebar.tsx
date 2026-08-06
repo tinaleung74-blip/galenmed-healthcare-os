e2f1090fbbe3cd685ef7af9e7e516d1c43e91bf3
YOUR NAME <jans103174@gmail.com>
2026-08-06 15:14:02 +0800

    Update

--- src/components/layouts/app-sidebar.tsx
+++ src/components/layouts/app-sidebar.tsx
@@ -14,6 +14,7 @@ import {
   Settings,
   Stethoscope,
   Users,
+  Settings2,
 } from "lucide-react"
 
 const navigation = [
@@ -61,6 +62,11 @@ const navigation = [
     name: "Reports",
     href: "/reports",
     icon: ChartNoAxesCombined,
+  },
+  {
+    name: "Settings",
+    href: "/settings",
+    icon: Settings2,
   },
 ]
 
